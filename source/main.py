953ec001ef0b7282fa1ec1187e0f1fe4a6de3dea
ValeriyMakarov <misteranon.com@gmail.com>
2024-12-26 18:36:43 +0200

    unfinished zoom, dragging squares

--- source/main.py
+++ source/main.py
@@ -1,3 +1,5 @@
+import math
+import random
 import sys
 import configparser
 import os
@@ -10,6 +12,43 @@ CONFIG_PATH: str = os.path.join(ROOT, 'config.ini')
 
 log = logging.getLogger()
 
+def get_k_zoom(x):
+    # todo при 0 зуме ~ 1. нужно найти другую формулу
+    return 10/(1+math.e**(-x+11/5))
+
+class BackgroundGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display_surface=pygame.display.get_surface()
+        self.offset = pygame.Vector2()
+
+    def draw(self, offset, zoom):
+        k_zoom = get_k_zoom(zoom)
+        self.offset.x = offset[0]
+        self.offset.y = offset[1]
+        for i in self.sprites():
+            if i.k_zoom != k_zoom:
+                i.scale(k_zoom)
+            self.display_surface.blit(
+                i.image,
+                i.rect.topleft+self.offset)
+
+
+class BackgroundElement(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.Surface([10, 10])
+        self.image.fill((random.randint(0, 255),random.randint(0, 255),random.randint(0, 255)))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+        self.k_zoom = 1
+
+    def scale(self, k_zoom):
+        self.image = pygame.transform.scale(
+            self.image,
+            (self.rect.width * k_zoom, self.rect.height * k_zoom)
+        )
+        self.rect = self.image.get_rect()
 
 def initialize_settings() -> None:
     config = configparser.ConfigParser()
@@ -38,12 +77,38 @@ def loop() -> None:
     clock = pygame.time.Clock()
     running = True
 
+    background = BackgroundGroup()
+    for i in range(150):
+        for j in range(100):
+            x=i*10
+            y=j*10
+            n=BackgroundElement(x,y)
+            background.add(n)
+    dragging = False
+    offset = (0,0)
+    zoom = 0
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                log.debug('QUIT action initialized.')
                 running = False
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                log.debug('Mouse button pressed.')
+                dragging = True
+            elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                log.debug('Mouse button unpressed.')
+                dragging = False
+            if event.type == pygame.MOUSEMOTION and dragging:
+                log.debug(f'Mouse motion.')
+                rel = event.rel
+                offset = (offset[0] + rel[0], offset[1] + rel[1])
+                log.debug(f'{offset=}')
+            if event.type == pygame.MOUSEWHEEL:
+                zoom += event.y
+                log.debug(f'{zoom=}')
 
-        screen.fill("purple")
+        screen.fill('white')
+        background.draw(offset, zoom)
         pygame.display.flip()
 
         clock.tick(60)
