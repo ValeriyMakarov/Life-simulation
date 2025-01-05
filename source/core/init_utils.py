1b662279b7f876e6c79caad6916ba0bfd72e216a
ValeriyMakarov <misteranon.com@gmail.com>
2025-01-05 14:24:59 +0200

    for marge

--- source/core/init_utils.py
+++ source/core/init_utils.py
@@ -6,20 +6,6 @@ from logging import Logger, getLogger, basicConfig, getLevelName
 
 
 class LoggerInitializer:
-    # __instance = None
-    #
-    # def __new__(cls, *args, **kwargs):
-    #
-    #     if not cls.__instance:
-    #         cls.__instance = super().__new__(cls)
-    #         logging.basicConfig(
-    #             level=logging.getLevelName(kwargs.get('level', 'debug').upper()),
-    #             datefmt='%y/%m/%d %H:%M:%S',
-    #             style='{',
-    #             format='[{asctime} {levelname}] : {module}.{funcName}\n\t{message}',
-    #             stream=sys.stdout
-    #         )
-    #     return logging.getLogger()
     log: Logger = None
 
     @classmethod
@@ -35,25 +21,6 @@ class LoggerInitializer:
 
 
 class ConfigInitializer:
-    # __instance = None
-    #
-    # def __new__(cls, *args, **kwargs):
-    #     config = None
-    #     if not cls.__instance:
-    #         cls.__instance = super().__new__(cls)
-    #         # указать вечный корень типа C:/Programs...
-    #         root = re.sub('(.*Life-simulation).*', r'\1', os.getcwd())
-    #         config_path = os.path.join(root, 'config.ini')
-    #         config = configparser.ConfigParser()
-    #
-    #         if not config.read(config_path):
-    #             config['logger'] = {
-    #                 'level': 'DEBUG',
-    #                 'log_folder': 'logs'
-    #             }
-    #             with open(config_path, 'w') as file:
-    #                 config.write(file)
-    #     return config
     config: configparser.ConfigParser
     config_path: str
 
--- source/main.py
+++ source/main.py
@@ -1,86 +1,86 @@
 import logging
-import random
-import pygame
+# import random
+# import pygame
 
 from core.init_utils import LoggerInitializer, ConfigInitializer
 from core.engine import Engine
 
 log: logging.Logger
 
-class BackgroundGroup(pygame.sprite.Group):
-    def __init__(self):
-        super().__init__()
-        self.display_surface=pygame.display.get_surface()
-        self.offset = pygame.Vector2()
-
-    def draw(self, offset):
-
-        self.offset.x = offset[0]
-        self.offset.y = offset[1]
-        for i in self.sprites():
-
-            self.display_surface.blit(
-                i.image,
-                i.rect.topleft+self.offset)
-
-
-class BackgroundElement(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        super().__init__()
-        self.image = pygame.Surface([10, 10])
-        self.image.fill((random.randint(0, 255),random.randint(0, 255),random.randint(0, 255)))
-        self.rect = self.image.get_rect()
-        self.rect.topleft = (x, y)
-        self.k_zoom = 1
-
-
+# class BackgroundGroup(pygame.sprite.Group):
+#     def __init__(self):
+#         super().__init__()
+#         self.display_surface=pygame.display.get_surface()
+#         self.offset = pygame.Vector2()
+#
+#     def draw(self, offset):
+#
+#         self.offset.x = offset[0]
+#         self.offset.y = offset[1]
+#         for i in self.sprites():
+#
+#             self.display_surface.blit(
+#                 i.image,
+#                 i.rect.topleft+self.offset)
+#
+#
+# class BackgroundElement(pygame.sprite.Sprite):
+#     def __init__(self, x, y):
+#         super().__init__()
+#         self.image = pygame.Surface([10, 10])
+#         self.image.fill((random.randint(0, 255),random.randint(0, 255),random.randint(0, 255)))
+#         self.rect = self.image.get_rect()
+#         self.rect.topleft = (x, y)
+#         self.k_zoom = 1
 
 
-def loop() -> None:
-    log.info('Starting game.')
-    pygame.init()
-    screen = pygame.display.set_mode((1280, 720))
-    clock = pygame.time.Clock()
-    running = True
 
-    background = BackgroundGroup()
-    for i in range(250):
-        for j in range(250):
-            x=i*10
-            y=j*10
-            n=BackgroundElement(x,y)
-            background.add(n)
 
-    dragging = False
-    offset = (0,0)
-    zoom = 0
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                log.debug('QUIT action initialized.')
-                running = False
-            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                log.debug('Mouse button pressed.')
-                dragging = True
-            elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
-                log.debug('Mouse button unpressed.')
-                dragging = False
-            if event.type == pygame.MOUSEMOTION and dragging:
-                log.debug(f'Mouse motion.')
-                rel = event.rel
-                offset = (offset[0] + rel[0], offset[1] + rel[1])
-                log.debug(f'{offset=}')
-            if event.type == pygame.MOUSEWHEEL:
-                zoom += event.y
-                log.debug(f'{zoom=}')
-
-        screen.fill('white')
-        background.draw(offset)
-        pygame.display.flip()
-
-        clock.tick(60)
-    pygame.quit()
-    log.info('Game stopped.')
+# def loop() -> None:
+#     log.info('Starting game.')
+#     pygame.init()
+#     screen = pygame.display.set_mode((1280, 720))
+#     clock = pygame.time.Clock()
+#     running = True
+#
+#     background = BackgroundGroup()
+#     for i in range(250):
+#         for j in range(250):
+#             x=i*10
+#             y=j*10
+#             n=BackgroundElement(x,y)
+#             background.add(n)
+#
+#     dragging = False
+#     offset = (0,0)
+#     zoom = 0
+#     while running:
+#         for event in pygame.event.get():
+#             if event.type == pygame.QUIT:
+#                 log.debug('QUIT action initialized.')
+#                 running = False
+#             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+#                 log.debug('Mouse button pressed.')
+#                 dragging = True
+#             elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+#                 log.debug('Mouse button unpressed.')
+#                 dragging = False
+#             if event.type == pygame.MOUSEMOTION and dragging:
+#                 log.debug(f'Mouse motion.')
+#                 rel = event.rel
+#                 offset = (offset[0] + rel[0], offset[1] + rel[1])
+#                 log.debug(f'{offset=}')
+#             if event.type == pygame.MOUSEWHEEL:
+#                 zoom += event.y
+#                 log.debug(f'{zoom=}')
+#
+#         screen.fill('white')
+#         background.draw(offset)
+#         pygame.display.flip()
+#
+#         clock.tick(60)
+#     pygame.quit()
+#     log.info('Game stopped.')
 
 # scene = pygame.rect.Rect((0,0),)
 
@@ -91,6 +91,4 @@ if __name__ == '__main__':
         ConfigInitializer.config.get('logger', 'level', fallback='INFO')
     )
     log = LoggerInitializer.log
-    log.info('12')
     engine = Engine()
-    # log.info(engine.scene_size())
